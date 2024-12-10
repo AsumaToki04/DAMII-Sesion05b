66b13118083941a8d3e9e907d81e66754ed175c3
AsumaToki04 <megamanzxu@hotmail.com>
2024-12-10 05:26:40 -0800

    Vista ContentView Modificada 1 y Entidad Escritor Modificada 1

--- Sesion05b/ContentView.swift
+++ Sesion05b/ContentView.swift
@@ -9,9 +9,34 @@ import SwiftUI
 import CoreData
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Escritor.nombre , ascending: true)],
+        animation: .default
+    ) private var escritores: FetchedResults<Escritor>
+    
     var body: some View {
-        VStack {
-            
+        NavigationView {
+            VStack {
+                if escritores.isEmpty {
+                    Text("No hay escritores")
+                        .font(.headline)
+                        .foregroundColor(.gray)
+                        .padding()
+                } else {
+                    ForEach(escritores) { item in
+                        HStack {
+                            let cantidadDeLibros = item.libros?.count ?? 0
+                            Text(item.nombre ?? "Sin nombre")
+                            Spacer()
+                            Text("\(cantidadDeLibros) libros")
+                                .foregroundColor(.gray)
+                                .font(.subheadline)
+                        }
+                    }
+                }
+            }
         }
+        .navigationTitle("Escritores")
     }
 }
